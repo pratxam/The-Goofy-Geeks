e074b08ae8f63854907be177ca19938552d7b081
vikas-pai <paivikas1402@gmail.com>
2022-12-23 16:34:07 +0530

    add club name in getAllEvents response

--- Backend/controllers/otherController.js
+++ Backend/controllers/otherController.js
@@ -3,11 +3,22 @@ import connection from '../database.js';
 
 //GET ALL EVENTS
 export const getallEvents = async (req, res, next) => {
+    console.log("get all events")
 
     try {
-        console.log("Hello World");
+        var clubIdMap = new Map();
+        connection.query("select * from club", (err, result) => {
+            if (err) throw new Error(err);
+            result.forEach(function (element) {
+                clubIdMap.set(element.Cid, element.Cname);
+            });
+        });
+
         connection.query("SELECT * FROM event", (err, result) => {
             if (err) throw err;
+            result.forEach(function (element) {
+                element.Cname = clubIdMap.get(element.C_id);
+            });
             res.status(200).json({
                 result
             })
