434e0a96648f9a34b595013dcf4a602118d956b0
vikas-pai <paivikas1402@gmail.com>
2023-01-14 15:25:59 +0530

    removed bg class

--- Backend/middlewares/checkUser.js
+++ Backend/middlewares/checkUser.js
@@ -6,19 +6,21 @@ const checkUser = async (req, res, next)=>{
     const token = req.cookies.token
     const userId_cookie = req.cookies.userId
     const adminId_cookie = req.cookies.adminId
-    if(!token || !userId_cookie){
+    console.log(token, userId_cookie, adminId_cookie);
+    if(token==null || userId_cookie==null){
         res.redirect('/login')
     }
-    
-    try {
-        const {userId} = jwt.verify(token, "goofygeeks");
-        console.log(userId, adminId_cookie, adminId_cookie != userId)
-        if(userId_cookie != userId || (adminId_cookie && adminId_cookie != userId)) {
-            res.redirect('/login')
+    else{
+        try {
+            const {userId} = jwt.verify(token, "goofygeeks");
+            console.log(userId, adminId_cookie, adminId_cookie != userId)
+            if(userId_cookie != userId || (adminId_cookie && adminId_cookie != userId)) {
+                res.redirect('/login')
+            }
+            next();
+        } catch (error) {
+            res.redirect('/login') 
         }
-        next();
-    } catch (error) {
-        res.redirect('/login') 
     }
 }
 
